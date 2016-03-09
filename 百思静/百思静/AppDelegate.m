b9ae3852dc6ad73f5f5db180e65807d7385f5667
Upliver <2639673289@qq.com>
2016-03-10 00:30:26 +0800

    删除不必要的文件,写好主流框架

--- 百思静/百思静/AppDelegate.m
+++ 百思静/百思静/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LJTabBarViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    
+    LJTabBarViewController *vc = [[LJTabBarViewController alloc] init];
+    
+    
+    self.window.rootViewController = vc;    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
